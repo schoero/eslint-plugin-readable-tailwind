786cbb505d1ff8b5d14f3a8104b58225cd5ab3a0
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-12-11 06:26:49 +0100

    fix: rule prefix

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,33 @@ import { tailwindSortClasses } from "eptm:rules:tailwind-sort-classes.js";
 import type { ESLint } from "eslint";
 
 
+const pluginName = "readable-tailwind";
+
 export default {
   configs: {
     "recommended/error": {
-      plugins: ["readable-tailwind"],
+      plugins: [pluginName],
       rules: {
-        [jsxAttributeExpression.name]: "error",
-        [tailwindNoUnnecessaryWhitespace.name]: "error",
-        [tailwindSortClasses.name]: "error",
-        [tailwindMultiline.name]: "error"
+        [`${pluginName}/${jsxAttributeExpression.name}`]: "error",
+        [`${pluginName}/${tailwindNoUnnecessaryWhitespace.name}`]: "error",
+        [`${pluginName}/${tailwindSortClasses.name}`]: "error",
+        [`${pluginName}/${tailwindMultiline.name}`]: "error"
       }
     },
     "recommended/warn": {
-      plugins: ["readable-tailwind"],
+      plugins: [pluginName],
       rules: {
-        [jsxAttributeExpression.name]: "warn",
-        [tailwindNoUnnecessaryWhitespace.name]: "warn",
-        [tailwindSortClasses.name]: "warn",
-        [tailwindMultiline.name]: "warn"
+        [`${pluginName}/${jsxAttributeExpression.name}`]: "warn",
+        [`${pluginName}/${tailwindNoUnnecessaryWhitespace.name}`]: "warn",
+        [`${pluginName}/${tailwindSortClasses.name}`]: "warn",
+        [`${pluginName}/${tailwindMultiline.name}`]: "warn"
       }
     }
   },
   rules: {
-    [jsxAttributeExpression.name]: jsxAttributeExpression.rule,
-    [tailwindNoUnnecessaryWhitespace.name]: tailwindNoUnnecessaryWhitespace.rule,
-    [tailwindSortClasses.name]: tailwindSortClasses.rule,
-    [tailwindMultiline.name]: tailwindMultiline.rule
+    [`${pluginName}/${jsxAttributeExpression.name}`]: jsxAttributeExpression.rule,
+    [`${pluginName}/${tailwindNoUnnecessaryWhitespace.name}`]: tailwindNoUnnecessaryWhitespace.rule,
+    [`${pluginName}/${tailwindSortClasses.name}`]: tailwindSortClasses.rule,
+    [`${pluginName}/${tailwindMultiline.name}`]: tailwindMultiline.rule
   }
 } satisfies ESLint.Plugin;
