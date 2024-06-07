a10836cc0d3ee2056be7f34eb6b25e3fe8c3f4f6
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2024-06-07 22:10:22 +0200

    fix: remove unnecessary plugin import in config.ts

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -17,7 +17,6 @@ export const config = {
       }
     },
     warning: {
-      plugins: [pluginName],
       rules: {
         [`${pluginName}/${tailwindNoUnnecessaryWhitespace.name}`]: "warn",
         [`${pluginName}/${tailwindSortClasses.name}`]: "warn",
