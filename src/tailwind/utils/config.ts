5d704802a8b290bed42ea272002a15fdcbe06708
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-01-29 23:24:40 +0100

    perf: cache config path

--- src/tailwind/utils/config.ts
+++ src/tailwind/utils/config.ts
@@ -2,11 +2,18 @@ import { existsSync } from "node:fs";
 import { basename, dirname, resolve } from "node:path";
 
 
+const CACHE = new Map<string, string>();
+
 export function findFileRecursive(cwd: string, paths: string[]) {
   const resolvedPaths = paths.map(p => resolve(cwd, p));
 
   for(let resolvedPath = resolvedPaths.shift(); resolvedPath !== undefined; resolvedPath = resolvedPaths.shift()){
+    if(CACHE.has(resolvedPath)){
+      return CACHE.get(resolvedPath);
+    }
+
     if(existsSync(resolvedPath)){
+      CACHE.set(resolvedPath, resolvedPath);
       return resolvedPath;
     }
 
