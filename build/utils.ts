478e10e1971e3521cde4a9f8e2e4e90bd4d6bbcb
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-01-30 20:43:58 +0100

    fix: throw build errors

--- build/utils.ts
+++ build/utils.ts
@@ -6,11 +6,10 @@ export async function $(
 ): Promise<string | Buffer> {
     return new Promise((resolve, reject) => {
         exec(String.raw(command, ...values), (error, stdout, stderr) => {
-            if (error && stderr) {
-                console.error(error, stderr)
-                reject(error)
+            if (error || stderr) {
+                reject(stderr || error)
             }
-            resolve(stdout || stderr)
+            resolve(stdout)
         })
     })
 }
