7f685dd02e98edf4d5a5d5fb680f3df456bd5547
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-01-30 23:00:09 +0100

    test: increase test timeout

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,9 @@
-export { config as default } from "@schoero/configs/vite";
+import { config } from "@schoero/configs/vite";
+
+import type { UserConfig } from "vitest/node";
+
+
+export default {
+  ...config,
+  testTimeout: 10_000
+} satisfies UserConfig;
