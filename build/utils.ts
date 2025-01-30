5ef94edb62ad2a74b9972ac9c7792f8241a90bf9
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-01-30 22:10:17 +0100

    fix: only reject if on error

--- build/utils.ts
+++ build/utils.ts
@@ -3,8 +3,8 @@ import { exec, type ExecOptions } from 'node:child_process'
 export async function $(command: string, options?: ExecOptions): Promise<string | Buffer> {
     return new Promise((resolve, reject) => {
         exec(command, options, (error, stdout, stderr) => {
-            if (error || stderr) {
-                reject(stderr || error)
+            if (error) {
+                reject(error || stderr)
             }
             resolve(stdout)
         })
