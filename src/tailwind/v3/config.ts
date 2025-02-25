ce1b93a93e108dd8cf37473a68a1bcfb1c83590b
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-02-25 20:04:46 +0100

    fix: don't throw error in v3 if config is not found

--- src/tailwind/v3/config.ts
+++ src/tailwind/v3/config.ts
@@ -10,11 +10,5 @@ export function findTailwindConfig(cwd: string, configPath?: string) {
     "tailwind.config.ts"
   ];
 
-  const path = findFileRecursive(cwd, potentialPaths);
-
-  if(!path){
-    throw new Error("Could not find a valid Tailwind CSS configuration");
-  }
-
-  return path;
+  return findFileRecursive(cwd, potentialPaths);
 }
