5f12cbc6437ec46340c2b6dd327c0cbe25c60da0
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-05-07 22:52:13 +0200

    wip: v3

--- src/tailwind/v3/conflicting-classes.ts
+++ src/tailwind/v3/conflicting-classes.ts
@@ -1,10 +1,14 @@
-import * as rules from "tailwindcss3/lib/lib/generateRules.js";
+import { } from "postcss/lib/expandApplyAtRules";
+import { isClean } from "postcss/lib/symbols";
+import * as generator from "tailwindcss3/lib/lib/generateRules.js";
 
 import { findTailwindConfig } from "./config.js";
 import { createTailwindContextFromConfigFile } from "./context.js";
 
 import type { ConfigWarning, GetConflictingClassesRequest, GetConflictingClassesResponse } from "../api/interface.js";
 
+
+export type ConflictingClasses = { [hierarchy: string]: { cssPropertyName: string; tailwindClassName: string; cssPropertyValue?: string; }[]; };
 
 export async function getConflictingClasses({ classes, configPath, cwd }: GetConflictingClassesRequest): Promise<GetConflictingClassesResponse> {
   const warnings: ConfigWarning[] = [];
@@ -20,11 +24,43 @@ export async function getConflictingClasses({ classes, configPath, cwd }: GetCon
 
   const context = createTailwindContextFromConfigFile(config?.path, config?.invalidate);
 
-  const conflictingClasses = classes
-    .filter(className => {
-      return (rules.generateRules?.([className], context) ?? rules.default?.generateRules?.([className], context)).length === 0;
-    });
+  const potentialConflicts: ConflictingClasses = {};
 
-  // @ts-expect-error - sdf
+  for(const className of classes){
+    const rules = generator.generateRules?.([className], context) ?? generator.default?.generateRules?.([className], context);
+
+    for(const [_, rule] of rules){
+      const isRoot = rule[isClean];
+      extractCssProperty(className, isRoot ? rule.nodes : [rule], potentialConflicts, "");
+    }
+  }
+
+  const conflictingClasses = Object.fromEntries(Object.entries(potentialConflicts).filter(([_, value]) => value.length > 1));
+
   return [conflictingClasses, warnings];
+}
+
+
+function extractCssProperty(className: string, nodes: any[], conflicts: ConflictingClasses, path: string): ConflictingClasses {
+  for(const node of nodes){
+    if(node.type === "decl"){
+      conflicts[`${path}${node.prop}`] ??= [];
+      conflicts[`${path}${node.prop}`].push({
+        cssPropertyName: node.prop,
+        cssPropertyValue: node.value,
+        tailwindClassName: className
+      });
+      continue;
+    }
+
+    if(node.type === "rule"){
+      return extractCssProperty(className, node.nodes, conflicts, path + node.selector);
+    }
+
+    if(node.type === "at-rule"){
+      return extractCssProperty(className, node.nodes, conflicts, path + node.name + node.params);
+    }
+  }
+
+  return conflicts;
 }
